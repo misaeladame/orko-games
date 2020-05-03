09effdecc16da801c921057a6d3269e50e4a2954
misaeladame <62737579+misaeladame@users.noreply.github.com>
2020-05-03 17:22:41 -0500

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,110 @@
+<div id="footer"> <!--  Inicia #footer-->
+    <div class="container"> <!--  Inicia container-->
+        <div class="row">
+            <div class="col-sm-6 col-md-3">
+
+                <h4>Paginas</h4>
+
+                <ul>
+                    <li><a href="cart.php">Carrito de compras</a></li>
+                    <li><a href="contact.php">Tienda</a></li>
+                    <li><a href="shop.php">Contactanos</a></li>
+                    <li><a href="checkout.php">Mi cuenta</a></li>
+                </ul>
+
+                <hr>
+
+                <h4>Sección de Usuario</h4>
+
+                <ul>
+                    <li><a href="checkout.php">Iniciar sesión</a></li>
+                    <li><a href="customer_register.php">Registrarse</a></li>
+      
+                
+
+                </ul>
+            
+                <hr class="hidden-md hidden-lg hidden-sm">
+
+            </div>
+
+            <div class="com-sm-6 col-md-3">
+
+                <h4>Categorías de productos principales</h4>
+
+                <ul>
+                    <li><a href="#">Cartas</a></li>
+                    <li><a href="#">Accesorios</a></li>
+                    <li><a href="#">Juegos de mesa</a></li>
+                    <li><a href="#">Modelismo</a></li>
+                </ul>
+
+                <hr class="hidden-md hidden-lg">
+
+            </div>
+
+            <div class="col-sm-6 col-md-3">
+
+                <h4>Encuentranos:</h4>
+
+                <p>
+
+                    <strong>Orko Games </strong>
+                    <br/>Av. Hidalgo
+                    <br/>Col. Centro
+                    <br/>8712755372
+                    <br/>orko.games@gmail.com
+                    <br/><strong>Nestor</strong>
+
+                </p>
+
+                <a href="contact.php">Checa nuestra página de contacto</a>
+
+                <hr class="hidden-md hidden-lg">
+           
+            </div>
+
+            <div class="col-sm-6 col-md-3">
+
+                <h4>Recibe las noticias</h4>
+
+                <p class="text-muted">
+                    No te pierdas nuestras últimas actualizaciones
+                </p>
+
+                <form action="" method="post">
+                    <div class="input-group">
+
+                        <input type="text" class="form-control" name="email">
+
+                        <span class="input-group-btn">
+
+                            <input type="submit" value="subscribe" class="btn btn-default">
+
+                        </span>
+
+                    </div>
+                </form>
+
+                <hr>
+
+                <h4>Mantenerse en contacto</h4>
+
+                <p class="social">
+                    <a href="https://es-la.facebook.com/OrkoGames/" class="fa fa-facebook"></a>
+                    <a href="#" class="fa fa-twitter"></a>
+                    <a href="#" class="fa fa-instagram"></a>
+                </p>
+
+            </div>
+        </div> 
+    </div> <!--  Termina container-->
+</div> <!--  Termina #footer -->
+
+<div id="copyright"> <!--  Inicia #copyright-->
+    <div class="container">
+         
+        <p>&copy; 2020 Orko Games Inc. Todos los derechos reservados.</p>
+
+    </div>
+</div> <!--  Termina #copyright-->
